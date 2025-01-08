3b38b4483970932f3e991ff47a14a0f9e68d0ef0
Yebapoo <baileychin08@gmail.com>
2025-01-07 16:58:36 -0800

    Update Pipe_In-Out_Action.java

--- Pipe_In-Out_Action.java
+++ Pipe_In-Out_Action.java
@@ -1,1 +1,11 @@
+/*define motor*/
+/*motor var type*/ pipeMotor = /*motor*/;
 
+public void motorRotation(Double angle){
+  pipeMotor.setSafetyEnabled(true);
+  pipeMotor.setSafetyEnabled(false);
+  pipeMotor.setExpiration(.1);
+  pipeMotor.setAngle(angle);
+  pipeMotor.feed();
+
+    }
